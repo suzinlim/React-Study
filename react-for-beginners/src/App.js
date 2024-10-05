ea96f05fccf2a6985c03f62b45e7adf9219bd605
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-10-05 22:37:31 +0900

    feat: React Router - useParams for dynamic routing

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="movie" element={<Detail />} />
+        <Route path="movie/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-    return <h1>Detail</h1>
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async () => {
+    const response = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(response);
+    setMovie(response.data.movie);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>Detail</h1>
+          <div>{movie.title}</div>
+          <img src={movie.medium_cover_image} />
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Detail;
